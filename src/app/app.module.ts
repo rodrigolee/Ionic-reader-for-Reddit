ba929d5d3d0065a78a030743042861e4e4e3e7b7
Rodrigo Lee <rodrigolee@Rodrigos-MBP.lan>
2018-03-12 16:05:53 -0700

    finished connecting controllers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,40 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { JsonpModule } from '@angular/http';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { HttpModule } from '@angular/http';
+import { YtProvider } from '../providers/yt/yt';
+import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player';
+import { PlaylistPage } from '../pages/playlist/playlist';
+// import { RedditApiServiceProvider } from '../providers/reddit-api-service/reddit-api-service';
+ 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    PlaylistPage,
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule,
+    JsonpModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    PlaylistPage,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    YtProvider,
+    YoutubeVideoPlayer,
+    // RedditApiServiceProvider,
+  ],
 })
 export class AppModule {}
